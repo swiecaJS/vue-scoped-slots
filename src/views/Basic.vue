8233a3e82dfc77ead502f0f313735497e7e17202
swiecaJS <karol.swieca@netguru.co>
2019-02-13 17:32:22 +0100

    basic example working

--- src/views/Basic.vue
+++ src/views/Basic.vue
@@ -3,15 +3,35 @@
     <h1>Basic usage</h1>
     <p>Alter display of a component</p>
     <UsersList/>
+    <UsersListWithSlot>
+      <template v-slot="{user}">
+        <div class="text--red text--heavy">{{user.name.first}} {{user.name.last}}</div>
+        <div>Age: {{user.dob.age}}</div>
+        <div>Phone:{{user.phone}}</div>
+      </template>
+    </UsersListWithSlot>
   </section>
 </template>
 
 <script>
 import UsersList from "@/components/Users/UsersList/UsersList.vue";
+import UsersListWithSlot from "@/components/Users/UsersListWithSlot/UsersListWithSlot.vue";
 export default {
   components: {
-    UsersList
+    UsersList,
+    UsersListWithSlot
   }
 };
 </script>
 
+<style lang="scss" scoped>
+.text {
+  &--red {
+    color: red;
+  }
+
+  &--heavy {
+    font-weight: 900;
+  }
+}
+</style>
