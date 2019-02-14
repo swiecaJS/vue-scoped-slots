6562b4bf3cfc11eafdb30177db03d43f430af9fe
swiecaJS <karol.swieca@netguru.co>
2019-02-14 08:56:44 +0100

    baisc example done

--- src/components/layout/TheHeader/TheHeader.vue
+++ src/components/layout/TheHeader/TheHeader.vue
@@ -16,7 +16,7 @@ export default {};
 </script>
 
 
-<style lang="scss" >
+<style lang="scss" scoped>
 .header {
   display: flex;
   background: #42b983;
--- src/views/Basic.vue
+++ src/views/Basic.vue
@@ -1,15 +1,28 @@
 <template>
   <section>
-    <h1>Basic usage</h1>
-    <p>Alter display of a component</p>
-    <UsersList/>
-    <UsersListWithSlot>
-      <template v-slot="{user}">
-        <div class="text--red text--heavy">{{user.name.first}} {{user.name.last}}</div>
-        <div>Age: {{user.dob.age}}</div>
-        <div>Phone:{{user.phone}}</div>
-      </template>
-    </UsersListWithSlot>
+    <div class="title">
+      <h1>Basic usage</h1>
+      <p>Alter display of a component</p>
+      <p>Access child props in parent</p>
+    </div>
+    <div class="examples">
+      <div class="example">
+        <pre>{{userListSyntax}}</pre>
+        <UsersList/>
+      </div>
+      <div class="example">
+        <div class="code-wrapper">
+          <pre>{{userListWithSlotsSyntax}}</pre>
+        </div>
+        <UsersListWithSlot>
+          <template v-slot="{user}">
+            <div class="text--red text--heavy">{{user.name.first}} {{user.name.last}}</div>
+            <div>Age: {{user.dob.age}}</div>
+            <div>Phone:{{user.phone}}</div>
+          </template>
+        </UsersListWithSlot>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -20,11 +33,43 @@ export default {
   components: {
     UsersList,
     UsersListWithSlot
+  },
+  data() {
+    return {
+      userListSyntax: `<UserList />`,
+      userListWithSlotsSyntax: `
+        <UsersListWithSlot>
+          <template v-slot="{user}">
+            <div class="text--red text--heavy">
+              {{user.name.first}} {{user.name.last}}
+            </div>
+            <div>Age: {{user.dob.age}}</div>
+            <div>Phone:{{user.phone}}</div>
+          </template>
+        </UsersListWithSlot>
+      `
+    };
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.title {
+  margin-top: 2em;
+  h1 {
+    margin: 0;
+  }
+
+  p {
+    margin: 0;
+    font-style: italic;
+  }
+}
+
+.examples {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
 .text {
   &--red {
     color: red;
@@ -33,5 +78,11 @@ export default {
   &--heavy {
     font-weight: 900;
   }
+}
+
+pre {
+  text-align: left;
+  display: inline-block;
+  min-height: 200px;
 }
 </style>
